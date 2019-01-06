bddbc2e7cbd15c291d8740e6e51af0273af5b4b8
lethithienhuong <44860862+lethithienhuong@users.noreply.github.com>
2019-01-06 10:07:04 +0700

    sửa giao diện form luân chuyển và thanh lý

--- TH_NET_Cuoi_Ky/TH_NET_Cuoi_Ky/GUI/ThanhLy.Designer.cs
+++ TH_NET_Cuoi_Ky/TH_NET_Cuoi_Ky/GUI/ThanhLy.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ThanhLy));
             this.button2 = new System.Windows.Forms.Button();
             this.bnt_OK = new System.Windows.Forms.Button();
             this.txt_TinhTrang = new System.Windows.Forms.TextBox();
@@ -50,22 +51,30 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(227, 275);
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(218, 275);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 28);
+            this.button2.Size = new System.Drawing.Size(84, 28);
             this.button2.TabIndex = 9;
             this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // bnt_OK
             // 
-            this.bnt_OK.Location = new System.Drawing.Point(18, 275);
+            this.bnt_OK.BackColor = System.Drawing.Color.Transparent;
+            this.bnt_OK.Image = ((System.Drawing.Image)(resources.GetObject("bnt_OK.Image")));
+            this.bnt_OK.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bnt_OK.Location = new System.Drawing.Point(21, 275);
             this.bnt_OK.Name = "bnt_OK";
-            this.bnt_OK.Size = new System.Drawing.Size(75, 29);
+            this.bnt_OK.Size = new System.Drawing.Size(72, 29);
             this.bnt_OK.TabIndex = 8;
             this.bnt_OK.Text = "OK";
-            this.bnt_OK.UseVisualStyleBackColor = true;
+            this.bnt_OK.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.bnt_OK.UseVisualStyleBackColor = false;
             this.bnt_OK.Click += new System.EventHandler(this.bnt_OK_Click);
             // 
             // txt_TinhTrang
@@ -78,6 +87,7 @@
             // label13
             // 
             this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
             this.label13.Location = new System.Drawing.Point(15, 233);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(78, 18);
@@ -109,6 +119,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Location = new System.Drawing.Point(15, 193);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
@@ -133,6 +144,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Location = new System.Drawing.Point(15, 49);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
@@ -156,6 +168,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Location = new System.Drawing.Point(15, 89);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
@@ -166,6 +179,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(15, 9);
             this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
@@ -185,6 +199,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Location = new System.Drawing.Point(15, 129);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
@@ -195,6 +210,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
             this.label11.Location = new System.Drawing.Point(18, 161);
             this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
@@ -223,6 +239,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(323, 329);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.numericUpDown_SL);
